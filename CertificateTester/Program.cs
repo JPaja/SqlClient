009e14afd24b780cabcd3d7653acffada5f6e50c
Jovan Pavlovic <a-jpavlovic@microsoft.com>
2021-09-06 13:49:16 +0200

    Add: Test

---
+++ CertificateTester/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Data.SqlClient;
+
+namespace CertificateTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CreateSqlConnectionWithClientCertificatePem();
+            CreateSqlConnectionWithClientCertificatePFX();
+            CreateSqlConnectionWithClientCertificatePFXConnectionString();
+        }
+
+       /* public static void CreateSqlConnectionWithClientCertificatePem()
+        {
+            Console.WriteLine();
+            Console.WriteLine(nameof(CreateSqlConnectionWithClientCertificatePem));
+            try
+            {
+                var cert = X509Certificate2.CreateFromEncryptedPemFile("cert.pem", "testpass123", "key.pem");
+                var csb = new SqlConnectionStringBuilder("Server=tcp:MDCSSQL-JOVPAV2,1453;Database=master"); //Trusted_Connection=False;MultipleActiveResultSets=true;
+                //csb.Authentication = SqlAuthenticationMethod.SqlCertificate;
+                using (var conn = new SqlConnection(csb.ConnectionString, cert))
+                using (var cmd = new SqlCommand("SELECT 1;", conn))
+                {
+                    conn.Open();
+                    var result = cmd.ExecuteScalar();
+                    if ((int)result == 1)
+                    {
+                        Console.WriteLine("OK");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Crash");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+*/
+        public static void CreateSqlConnectionWithClientCertificatePFXConnectionString()
+        {
+            Console.WriteLine();
+            Console.WriteLine(nameof(CreateSqlConnectionWithClientCertificatePFXConnectionString));
+            try
+            {
+                var certPath = @"F:\Sources\SqlClient-jovan\CertificateTester\bin\Debug\net5.0\hybrid-cert-a-1.pfx";
+                var key = "Yukon900";
+                var csb = new SqlConnectionStringBuilder($"Server=tcp:MDCSSQL-JOVPAV2,1453;Database=master;ClientCertificate={certPath};ClientKeyPassword={key};Authentication=SqlCertificate"); 
+                using (var conn = new SqlConnection(csb.ConnectionString))
+                using (var cmd = new SqlCommand("SELECT 1;", conn))
+                {
+                    conn.Open();
+                    var result = cmd.ExecuteScalar();
+                    if ((int)result == 1)
+                    {
+                        Console.WriteLine("OK");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Crash");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        public static void CreateSqlConnectionWithClientCertificatePFX()
+        {
+            Console.WriteLine();
+            Console.WriteLine(nameof(CreateSqlConnectionWithClientCertificatePFX));
+            try
+            {
+                var certPath = @"F:\Sources\SqlClient-jovan\CertificateTester\bin\Debug\net5.0\hybrid-cert-a-1.pfx";
+                var key = "Yukon900";
+                var cert = new X509Certificate2(certPath, key);
+                var csb = new SqlConnectionStringBuilder($"Server=tcp:MDCSSQL-JOVPAV2,1453;Database=master;");
+                using (var conn = new SqlConnection(csb.ConnectionString, cert))
+                using (var cmd = new SqlCommand("SELECT 1;", conn))
+                {
+                    conn.Open();
+                    var result = cmd.ExecuteScalar();
+                    if ((int)result == 1)
+                    {
+                        Console.WriteLine("OK");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Crash");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Data.SqlClient/tests/ManualTests/SQL/SqlCredentialTest/SqlCertificateTest.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using System.Security;
+using System.Threading;
+using Xunit;
+
+namespace Microsoft.Data.SqlClient.ManualTesting.Tests
+{
+    public static class SqlCertificateTest
+    {
+
+        [Fact]
+        public static void CreateSqlConnectionWithClientCertificateStringPFX()
+        {
+            try
+            {
+                var csb = new SqlConnectionStringBuilder("Server=tcp:MDCSSQL-JOVPAV2, 1453;Database=master;User Id=sa;password=Yukon900;"); //Trusted_Connection=False;MultipleActiveResultSets=true;
+                using (var conn = new SqlConnection(csb.ConnectionString))
+                using (var cmd = new SqlCommand("SELECT 1;", conn))
+                {
+                    conn.Open();
+                    Assert.Equal(1, cmd.ExecuteScalar());
+                }
+            }
+            finally
+            {
+            }
+        }
+    }
+}
